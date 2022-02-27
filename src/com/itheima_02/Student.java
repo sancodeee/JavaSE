72e06a66da3e1d6f2d5893a267b2331df4e859e2
sancodeee <1376474892@qq.com>
2022-02-27 14:25:31 +0800

    JavaSE基础练习demo

--- src/com/itheima_02/Student.java
+++ src/com/itheima_02/Student.java
@@ -1,5 +1,7 @@
 package com.itheima_02;
 
+import java.util.Objects;
+
 public class Student {
     private String name;
     private int age;
@@ -29,10 +31,26 @@ public class Student {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
     public String toString() {
         return "Student{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+
+
     }
 }

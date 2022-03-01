34aa9538247f2a39731d589aa91d749551e9d979
sancodeee <1376474892@qq.com>
2022-03-01 17:43:01 +0800

    JavaSE基础练习demo

---
+++ src/com/itheima03/threadTest03.java
@@ -0,0 +1,30 @@
+package com.itheima03;
+
+
+
+public class threadTest03 implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 200; i++) {
+            System.out.println("---------线程体执行中--------"+i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        创建实现Runable接口类的对象
+        threadTest03 threadtest = new threadTest03();
+
+//        创建线程对象,然后将threadtest抛入线程类对象中
+        Thread th = new Thread(threadtest);
+
+//        通过线程类对象启动线程
+        th.start();
+//
+
+        for (int i = 0; i < 200; i++) {
+            System.out.println("---------主线程执行中----------"+i);
+        }
+    }
+}

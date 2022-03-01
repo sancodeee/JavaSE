96d1a11e24f96157bd3bd34a09d97d2a0a63d7c4
sancodeee <1376474892@qq.com>
2022-03-01 12:41:22 +0800

    JavaSE基础练习demo

---
+++ src/com/itheima03/threadTest02.java
@@ -0,0 +1,54 @@
+package com.itheima03;
+
+//实现多线程下载器
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class threadTest02 extends Thread {
+
+    private String url;   //网络图片地址
+    private String name;   //保存文件名
+
+    public threadTest02(String url, String name) {
+        this.name = name;
+        this.url = url;
+    }  //带参构造方法
+
+    //    线程执行体
+    @Override
+    public void run() {
+        webDonloader wD = new webDonloader();
+        wD.downloader(url, name);
+        System.out.println("下载了文件"+name);
+    }
+
+    //    下载器
+    class webDonloader {
+        public void downloader(String url, String name) {
+            try {
+                FileUtils.copyURLToFile(new URL(url), new File(name));
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("io异常，downloader方法出现异常");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        创建线程对象
+        threadTest02 t1 = new threadTest02("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201701%2F28%2F20170128215011_2Fh8T.png&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1648692836&t=990d238f51723a8fc30796aa2c09b857", "1.jpg");
+        threadTest02 t2 = new threadTest02("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi2.hdslb.com%2Fbfs%2Farchive%2F9ee06b6735d4058cf9ede0ce8d0308d31a2d3e21.jpg&refer=http%3A%2F%2Fi2.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1648692836&t=f1e258acf8541ddc146e4960837567a8", "2.jpg");
+        threadTest02 t3 = new threadTest02("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202001%2F06%2F20200106165415_ehhmm.jpg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1648692836&t=de893c505b03403d36cc2fcb01ab1910", "3.jpg");
+
+        t1.start();  //运行线程
+        t2.start();
+        t3.start();
+    }
+
+
+}
